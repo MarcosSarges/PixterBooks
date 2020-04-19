a39847f97c4db4c3c6ba0d2074ca91df66e491d8
Marcos Sarges <dev.marcos.sarges@gmail.com>
2020-04-19 10:54:31 -0300

    type result getbooks

--- src/services/API/index.ts
+++ src/services/API/index.ts
@@ -1,12 +1,13 @@
 import Axios from 'axios';
 import Defines from '@services/Defines';
+import {BOOKS} from 'src/ts/types';
 
 const API = Axios.create({
   baseURL: Defines.API.HOST,
 });
 
 export const getBooks = async (query: string) => {
-  return await API.get(`${Defines.API.BOOKS}?q=${query}`);
+  return await API.get<BOOKS>(`${Defines.API.BOOKS}?q=${query}`);
 };
 
 export default API;
