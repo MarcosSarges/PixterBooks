947cf61847b04bd6717c94cd4c91ef9c65b02b03
Marcos Sarges <dev.marcos.sarges@gmail.com>
2020-04-20 08:57:38 -0300

    config redux

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,16 @@ import {NavigationContainer} from '@react-navigation/native';
 import HomeRouter from '@router/HomeRouter';
 import {StatusBar} from 'react-native';
 import colors from '@assets/styles/colors';
+import {Provider} from 'react-redux';
+import store from './store';
 
 const App = () => (
-  <>
+  <Provider store={store}>
     <StatusBar backgroundColor={colors.primary} barStyle="dark-content" />
     <NavigationContainer>
       <HomeRouter />
     </NavigationContainer>
-  </>
+  </Provider>
 );
 
 export default App;
