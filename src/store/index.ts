1de0427ad0bfdab48bc518f10dd9a6fb9ef7d75a
Marcos Sarges <dev.marcos.sarges@gmail.com>
2020-04-20 12:03:01 -0300

    config saga

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,10 +2,12 @@ import {createStore, applyMiddleware} from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import rootSaga from './sagas';
 import {rootReducer} from './reducer';
+//@ts-ignore
+import logger from 'redux-logger';
 
 const sagaMiddleware = createSagaMiddleware();
 
-const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
+const store = createStore(rootReducer, applyMiddleware(logger, sagaMiddleware));
 
 sagaMiddleware.run(rootSaga);
 
--- src/store/sagas.ts
+++ src/store/sagas.ts
@@ -1,4 +1,4 @@
-import {all, put, call, takeEvery} from 'redux-saga/effects';
+import {all, put, call, takeLatest, debounce} from 'redux-saga/effects';
 import {getBooks} from '@lib/API';
 import dicionario from '@assets/palavras';
 import constants from './constants';
@@ -22,8 +22,8 @@ function* asyncGetBookRefresh() {
 
 export default function* rootSaga() {
   yield all([
-    takeEvery(constants.ASYNC_GET_BOOK_ACTION, asyncGetBook),
-    takeEvery(constants.ASYNC_GET_BOOK_ACTION_NEXT_PAGE, asyncGetBookNextPage),
-    takeEvery(constants.ASYNC_GET_BOOK_ACTION_REFRESH, asyncGetBookRefresh),
+    takeLatest(constants.ASYNC_GET_BOOK_ACTION, asyncGetBook),
+    takeLatest(constants.ASYNC_GET_BOOK_ACTION_NEXT_PAGE, asyncGetBookNextPage),
+    takeLatest(constants.ASYNC_GET_BOOK_ACTION_REFRESH, asyncGetBookRefresh),
   ]);
 }
